52f4036e01c17b60435a37642e67d9f826162d40
naya460 <76655448+naya460@users.noreply.github.com>
2023-01-12 19:56:12 +0900

    Divide file about OpenFile function

--- src/brainxx.c
+++ src/brainxx.c
@@ -1,31 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
 
+#include "file.h"
 #include "error.h"
-
-char *OpenFile(const char *path){
-    // open file
-    FILE *file;
-    file = fopen(path, "r");
-    if (file  == NULL) Error(2, "failed to open the file\n");
-
-    // get file size
-    struct stat status;
-    if (stat(path, &status) != 0) Error(2, "failed to get file size\n");
-    long size = status.st_size;
-
-    // read file
-    char *str = (char*)malloc(sizeof(char) * (size + 1));
-    int read_count = fread(str, sizeof(char), size + 1, file);
-    if (read_count == 0) Error(2, "failed to read file\n");
-    str[read_count + 1] = '\0';
-
-    // close file
-    fclose(file);
-
-    return str;
-}
 
 void Compile(char *program){
     while(*program != '\0') {
---
+++ src/file.c
@@ -0,0 +1,30 @@
+#include "file.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+#include "error.h"
+
+char *OpenFile(const char *path){
+    // open file
+    FILE *file;
+    file = fopen(path, "r");
+    if (file  == NULL) Error(2, "failed to open the file\n");
+
+    // get file size
+    struct stat status;
+    if (stat(path, &status) != 0) Error(2, "failed to get file size\n");
+    long size = status.st_size;
+
+    // read file
+    char *str = (char*)malloc(sizeof(char) * (size + 1));
+    int read_count = fread(str, sizeof(char), size + 1, file);
+    if (read_count == 0) Error(2, "failed to read file\n");
+    str[read_count + 1] = '\0';
+
+    // close file
+    fclose(file);
+
+    return str;
+}
