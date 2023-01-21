7504eb8d7719dc431b603f7b6dde7b6587f4b9f5
naya460 <76655448+naya460@users.noreply.github.com>
2023-01-21 21:50:53 +0900

    Divide file about the Consume Function

--- include/compiler/compiler.h
+++ include/compiler/compiler.h
@@ -1,14 +1,6 @@
 #pragma once
 
-#include <stdbool.h>
-
-// Consume char and call needed function
-// 1. Compare current program and expected char
-// 2. If current program doesn't match expected char, return false
-// 3. Move program pointer to next char
-// 4. Call the function with program_ptr
-// 5. Return true
-bool Consume(char **program_ptr, char expect, void function(char **program_ptr));
+#include "compiler/consume.h"
 
 // Compile program
 void Compile(char *program);
--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -8,13 +8,6 @@ static char *program;
 
 // ========================================
 
-bool Consume(char **program_ptr, char expect, void function(char **program)){
-    if (**program_ptr != expect) return false;
-    ++(*program_ptr);
-    function(program_ptr);
-    return true;
-}
-
 void Compile(char *program_text){
     program = program_text;
 
