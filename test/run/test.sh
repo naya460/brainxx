b14ed763c0594a3f9431c67460d1baf40704fa3f
naya460 <76655448+naya460@users.noreply.github.com>
2023-02-02 12:08:07 +0900

    Perform Stack Push Running Tests

--- test/run/test.sh
+++ test/run/test.sh
@@ -31,7 +31,19 @@ expect() {
     fi
 }
 
-# tests - base
-set_sd base
+# tests - 001-base
+set_sd 001-base
 expect 001-empty.xx 0
 expect 002-ret.xx 1
+
+# tests - 002-stack - 001-push
+set_sd 002-stack/001-push
+expect 001-zero.xx 0
+expect 002-one.xx 1
+expect 003-nine.xx 9
+expect 004-ten.xx 10
+expect 005-3digit.xx 253
+expect 006-uint-overflow.xx 0
+
+# tests - 002-stack - 002-add
+set_sd 002-stack/002-add
