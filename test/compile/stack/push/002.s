1a93e0aa63c969e1c72314ff9eb0fa7536547339
naya460 <76655448+naya460@users.noreply.github.com>
2023-06-09 23:19:21 +0900

    Remove compile test

--- test/compile/stack/push/002.s
+++
@@ -1,9 +0,0 @@
-.intel_syntax noprefix
-.globl main
-main:
-    mov rbp, rsp
-    push 1234567890
-    mov rsp, rbp
-    mov rax, 0
-    ret
-
--- test/test.sh
+++ test/test.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-# compile test
-echo "=== Compile test ==="
-./test/compile/test.sh
-if [ $? -ne 0 ]; then
-    echo "Compile Test is faild"
-    exit 1
-fi
-
 # run test
-echo "=== Run test ==="
 ./test/run/test.sh
 if [ $? -ne 0 ]; then
-    echo "Run Test is faild"
     exit 1
 fi
 
