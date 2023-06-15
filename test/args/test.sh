4f89759de3e0d54b6cac34b1ebc8a048a90a8022
naya460 <76655448+naya460@users.noreply.github.com>
2023-06-15 22:52:28 +0900

    Create the tests about using args

---
+++ test/args/test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+assembler=gcc
+
+cd $(dirname $0)
+bxx=../../build/src/bxx
+
+is_ok() {
+    if [ $? -eq $1 ]; then
+        echo "-> ok"
+    else
+        echo "-> failed"
+        exit 1
+    fi
+}
+
+# no args
+echo "* no args"
+$bxx
+is_ok 1
+
+# no arg after -o
+echo "* no arg after -o"
+$bxx -o
+is_ok 1
+
+# compile with input file and output file
+echo "* compile with input file and output file"
+$bxx ret0.xx -o tmp.s
+is_ok 0
+
+# compile with other args order
+echo "* compile with other args order"
+$bxx -o tmp.s ret0.xx
+is_ok 0
+
+# 2 of input files
+echo "* 2 of input files"
+$bxx ret0.xx ret1.xx -o tmp.s
+is_ok 0
+$assembler tmp.s -o tmp.elf
+./tmp.elf
+is_ok 1
+
+# 2 of output files
+echo "* 2 of output files"
+$bxx ret0.xx -o tmp1.s -o tmp2.s
+is_ok 0
+$assembler tmp2.s -o tmp.elf
+./tmp.elf
+is_ok 0
--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
+# args test
+echo "*** args test ***"
+./test/args/test.sh
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+echo ""
+echo "=> args test : ok"
+echo ""
+
 # run test
+echo "*** run test ***"
 ./test/run/test.sh
 if [ $? -ne 0 ]; then
     exit 1
 fi
-
 echo ""
+echo "=> run test : ok"
+echo ""
+
 echo "*** All tests are successful ***"
